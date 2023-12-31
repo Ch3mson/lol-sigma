a875d36ca0ee25f1fd59ff69239d2d0e7afbab86
Ch3mson <bensonyan778@hotmail.com>
2023-12-30 18:35:52 -0800

    idk

--- lol-timeline/src/App.jsx
+++ lol-timeline/src/App.jsx
@@ -6,15 +6,18 @@ import Overview from './components/overview';
 function App() {
 
   const [searchText, setSearchText] = useState("");
+  const [tagLine, setTagLine] = useState("");
   const API_KEY = "RGAPI-e3059c24-7b56-43d0-bba3-8f6870da3c2d";
   const [playerData, setPlayerData] = useState({});
 
+
   function searchForPlayer(event) {
     
-    var APICallString = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + API_KEY;
+    var APICallString = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + searchText + "/" + tagLine + "?api_key=" + API_KEY;
     console.log(APICallString);
 
-    axios.get(APICallString).then(function (response) {
+
+    axios.get(APICallString, { 'headers' : { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}  } ).then(function (response) {
       //success:
       console.log(response.data);
       setPlayerData(response.data);
@@ -29,16 +32,23 @@ function App() {
 
       <div className="flex flex-col items-center p-3">
         <>
-          <div className="flex">
+          <div className="flex flex-row space-x-2">
             <input 
               type="text" 
-              className="py-2 px-4 rounded-l bg-[#25234a] text-white focus:outline-none w-80 h-12" // Set height with h-12
+              className="py-5 px-4 rounded bg-[#25234a] text-white focus:outline-none w-80 h-12" // Set height with h-12
               placeholder="Enter Summoner Name"
               onChange={e => setSearchText(e.target.value)}
             />
+
+            <input 
+              type="text" 
+              className="py-2 px-4 rounded bg-[#25234a] text-white focus:outline-none w-40 h-12" // Set height with h-12
+              placeholder="Tagline #"
+              onChange={e => setTagLine(e.target.value)}
+            />
             
             <button 
-              className="py-2 px-4 rounded-r bg-[#25234a] text-white hover:bg-[#444087] focus:outline-none h-12" // Set height with h-12
+              className="py-2 px-4 rounded bg-[#25234a] text-white hover:bg-[#444087] focus:outline-none h-12" // Set height with h-12
               onClick={e => searchForPlayer(e)}
             >
               Search for player
