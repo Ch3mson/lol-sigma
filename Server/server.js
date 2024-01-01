a110026f791a3188117631af16134b90a35815d7
Ch3mson <bensonyan778@hotmail.com>
2024-01-01 00:05:01 -0800

    Added backend requests to Riot API

--- Server/server.js
+++ Server/server.js
@@ -1,36 +1,43 @@
 var express = require('express')
 var cors = require('cors')
 const axios = require('axios');
+require('dotenv').config();
+
 
 var app = express()
 app.use(cors());
 
-
-const API_KEY = "RGAPI-222e6187-3ef8-4855-847e-8b0361697b71";
+const API_KEY = process.env.API_KEY;
 
 function getPlayerPUUID(playerName, playerTag) {
     return axios.get("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + playerName + "/" + playerTag + "?api_key=" + API_KEY)
         .then(response => {
             console.log(response.data);
             return response.data;
-        }).catch(err => err);
-}
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
 function getMatches(puuid, count) {
     return axios.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=" + count + "&api_key=" + API_KEY)
         .then(response => {
             console.log(response.data);
             return response.data;
-        }).catch(err => err);
-}
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
 function getUserData(puuid) {
     return axios.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + puuid + "?api_key=" + API_KEY)
         .then(response => {
             console.log(response.data);
             return response.data;
-        }).catch(err => err);
-}
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
 app.get('/user', async (req, res) => {
     const name = req.query.name;
